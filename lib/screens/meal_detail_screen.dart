6bd64994afc1e46108bd434c0088b4b19c2b39e2
Manish Verma <vermam318@gmail.com>
2021-03-15 17:23:46 +0530

    meal_detail_screen file created

---
+++ lib/screens/meal_detail_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class MealDetailScreen extends StatelessWidget {
+  static const routeName = '/meal_detail';
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('testing meals'),);
+  }
+}
