5ad7faa76ce2b5c070f652d0ec29dc74804eaa3b
Manish Verma <vermam318@gmail.com>
2021-03-16 20:45:15 +0530

    image wiget added

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -1,9 +1,33 @@
 import 'package:flutter/material.dart';
 
+import '../dummy_data.dart';
+
 class MealDetailScreen extends StatelessWidget {
   static const routeName = '/meal_detail';
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('testing meals'),);
+    final mealId = ModalRoute.of(context).settings.arguments as String;
+    final selectedMeal = DUMMY_MEALS.firstWhere((meal) => meal.id == mealId);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('${selectedMeal.title}'),
+      ),
+      body: Column(
+        children: [
+          Container(
+            height: 300,
+            width: double.infinity,
+            child: Image.network(
+              selectedMeal.imageUrl,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.all(10),
+            child: Text('Ingredients', style: Theme.of(context).textTheme.bodyText1,),
+          ),
+        ],
+      ),
+    );
   }
 }
