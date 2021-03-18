e412698db0276b0e09307c6da155e215701cb8d7
Manish Verma <vermam318@gmail.com>
2021-03-18 17:28:00 +0530

    Filter screen route added

---
+++ lib/widgets/main_drawer.dart
@@ -0,0 +1,68 @@
+import 'package:MealsApp/screens/filters_screen.dart';
+import 'package:flutter/material.dart';
+
+class MainDrawer extends StatelessWidget {
+  Widget buildListTile(String title, IconData icon, Function onTapHandler) {
+    return ListTile(
+      leading: Icon(
+        icon,
+        size: 26,
+      ),
+      title: Text(
+        title,
+        style: TextStyle(
+          fontFamily: 'RobotoCondensed',
+          fontSize: 24,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      onTap: onTapHandler,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: [
+          Container(
+            height: 120,
+            width: double.infinity,
+            alignment: Alignment.centerLeft,
+            padding: const EdgeInsets.all(20),
+            color: Theme.of(context).accentColor,
+            child: Text(
+              'Cooking Up!',
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w900,
+                color: Theme.of(context).primaryColor,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          buildListTile(
+            'Meals',
+            Icons.restaurant,
+            () {
+              Navigator.of(context).pushNamed('/');
+            }
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          buildListTile(
+            'Filters',
+            Icons.settings,
+            (){
+              Navigator.of(context).pushNamed(FiltersScreen.routeName);
+              
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
