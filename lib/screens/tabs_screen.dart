9d449ed42c77597f5e539ca41d2edc9f9a1b2719
Manish Verma <vermam318@gmail.com>
2021-03-21 09:33:46 +0530

    added constructor and init state for favoriteMeals

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -3,24 +3,33 @@ import 'package:flutter/material.dart';
 import './categories_screen.dart';
 import './favourite_screen.dart';
 import '../widgets/main_drawer.dart';
+import '../models/meal.dart';
 
 class TabsScreen extends StatefulWidget {
+  final List<Meal> favoriteMeals;
+  TabsScreen(this.favoriteMeals);
   @override
   _TabsScreenState createState() => _TabsScreenState();
 }
 
 class _TabsScreenState extends State<TabsScreen> {
-  final List<Map<String, Object>> _pages = [
+  List<Map<String, Object>> _pages;
+  int _selectedPageIndex = 0;
+
+  @override
+  void initState() {
+    _pages = [
     {
       'page': CategoriesScreen(),
       'title': 'Categories',
     },
     {
-      'page': FavoritesScreen(),
+      'page': FavoritesScreen(widget.favoriteMeals),
       'title': 'Your Favorites',
     },
   ];
-  int _selectedPageIndex = 0;
+    super.initState();
+  }
   void _selectPage(int index) {
     setState(() {
       _selectedPageIndex = index;
