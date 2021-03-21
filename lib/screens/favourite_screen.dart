1a5753d3987fac3883fdcd39976f9f110892c2a4
Manish Verma <vermam318@gmail.com>
2021-03-21 09:31:22 +0530

    favorite screen update with widgets

--- lib/screens/favourite_screen.dart
+++ lib/screens/favourite_screen.dart
@@ -1,10 +1,31 @@
+import 'package:MealsApp/widgets/meal_item.dart';
 import 'package:flutter/material.dart';
 
+import '../models/meal.dart';
 class FavoritesScreen extends StatelessWidget {
+  final List<Meal> favoriteMeals;
+  FavoritesScreen(this.favoriteMeals);
   @override
   Widget build(BuildContext context) {
+    if(favoriteMeals.isEmpty){
     return Center(
-      child: Text('The Favorites'),
+      child: Text('You have no favorites yet - start adding some!!'),
     );
+    }
+    else{
+      return ListView.builder(
+          itemBuilder: (ctx, index) {
+            return MealItem(
+              id: favoriteMeals[index].id,
+              title: favoriteMeals[index].title,
+              affordability: favoriteMeals[index].affordability,
+              complexity: favoriteMeals[index].complexity,
+              imageUrl: favoriteMeals[index].imageUrl,
+              duration: favoriteMeals[index].duration,
+            );
+          },
+          itemCount: favoriteMeals.length,
+        );
+    }
   }
 }
