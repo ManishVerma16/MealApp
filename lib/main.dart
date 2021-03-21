e191ba4081fa383c3b555f56efd3d0f4828ef71c
Manish Verma <vermam318@gmail.com>
2021-03-21 09:36:21 +0530

    multiple functions added, state & routes updated

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,8 @@ import './screens/category_meals_screen.dart';
 import './screens/tabs_screen.dart';
 import './screens/meal_detail_screen.dart';
 import './screens/filters_screen.dart';
+import './dummy_data.dart';
+import './models/meal.dart';
 
 // import 'screens/categories_screen.dart';
 
@@ -12,7 +14,60 @@ void main(){
   runApp(MyApp()); 
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Map<String, bool> _filters = {
+    'gluten': false,
+    'lactose': false,
+    'vegan': false,
+    'vegetarian': false,
+  };
+  List<Meal> _availableMeals = DUMMY_MEALS;
+  List<Meal> _favoriteMeals = [];
+
+  void _setFilters(Map<String, bool> filterData){
+    setState(() {
+      _filters = filterData;
+      _availableMeals = DUMMY_MEALS.where((meal) {
+        if(_filters['gluten'] && !meal.isGlutenFree){
+          return false;
+        }
+        if(_filters['lactose'] && !meal.isLactoseFree){
+          return false;
+        }
+        if(_filters['vegan'] && !meal.isVegan){
+          return false;
+        }
+        if(_filters['vegetarian'] && !meal.isVegetarian){
+          return false;
+        }
+        return true;
+      }).toList();
+    });
+  }
+    
+  void _toggleFavorite(String mealId){
+      final existingIndex = _favoriteMeals.indexWhere((meal) => meal.id== mealId);
+      if(existingIndex >=0 ){
+        setState(() {
+          _favoriteMeals.removeAt(existingIndex);
+        });
+      }
+      else{
+        setState(() {
+          _favoriteMeals.add(DUMMY_MEALS.firstWhere((meal) => meal.id==mealId));
+        });
+      }
+    }
+
+  bool _isMealFavorite(String id){
+      return _favoriteMeals.any((meal) => meal.id==id);
+    }
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -40,12 +95,12 @@ class MyApp extends StatelessWidget {
       // home: CategoriesScreen(),
       initialRoute: '/', // points to default route '/' but can be change to some other page
       routes: {
-        '/' : (ctx) => TabsScreen(),  // Annotation for home route
+        '/' : (ctx) => TabsScreen(_favoriteMeals),  // Annotation for home route
 
         // '/' : (ctx) => CategoriesScreen(),  // Annotation for home route
-        CategoryMealsScreen.routeName : (ctx) => CategoryMealsScreen(), // route name created as a static const property
-        MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
-        FiltersScreen.routeName: (ctx) => FiltersScreen(),
+        CategoryMealsScreen.routeName : (ctx) => CategoryMealsScreen(_availableMeals), // route name created as a static const property
+        MealDetailScreen.routeName: (ctx) => MealDetailScreen(_toggleFavorite, _isMealFavorite),
+        FiltersScreen.routeName: (ctx) => FiltersScreen(_filters, _setFilters),
       },
     );
   }
